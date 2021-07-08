6f4e64ff03ba9fb65795b8e7530f1c0e46643654
Ricardo Villa Sanchez <ing_ricardo.villa@hotmail.com>
2021-07-07 23:16:11 -0500

    Productivity view development!

--- js/app/productivity.js
+++ js/app/productivity.js
@@ -1,4 +1,12 @@
 var dataTable = null;
+var ctx2 = null;
+var chart = null;
+
+Date.prototype.addDays = function (days) {
+   var date = new Date(this.valueOf());
+   date.setDate(date.getDate() + days);
+   return date;
+}
 
 $.ajax({
    url: 'https://dashboard.8thsensus.com:8080/message',
@@ -21,7 +29,7 @@ $.ajax({
 
       usersData.forEach(user => {
          slctUsersHtml += `
-            <option value="${ user.userid }">${ user.userid }</option>
+            <option value="${user.userid}">${user.userid}</option>
          `;
       });
 
@@ -40,147 +48,87 @@ function getproductivityData() {
       headers: {
          "Content-Type": "application/x-www-form-urlencoded",
       },
-      type: "GET" /* or type:"GET" or type:"PUT" */,
+      type: "GET",
       dataType: "json",
       data: {},
       success: function (result) {
-         let dateFrom = document.getElementById("dateFrom").value;
-         let dateTo = document.getElementById("dateTo").value;
+         let slctDateRange = document.getElementById("slctDateRange").value == "" ? 1
+            : document.getElementById("slctDateRange").value;
          let slctUserId = document.getElementById("slctUserId").value;
-
-         Date.prototype.addDays = function(days) {
-            var date = new Date(this.valueOf());
-            date.setDate(date.getDate() + days);
-            return date;
-         }
-
-         if (dateFrom == "" && dateTo == "") {
-            document.getElementById("dateFrom").valueAsDate = new Date().addDays(-1);
-            document.getElementById("dateTo").valueAsDate = new Date();
-            dateFrom = document.getElementById("dateFrom").value;
-            dateTo = document.getElementById("dateTo").value;
-         }
+         let dateFrom = formatDate(new Date().addDays(-slctDateRange));
+         let dateTo = formatDate(new Date());
+         let timeInterval = getTimeInterval(slctDateRange);
+         let timeIntervalFormat = parseInt(slctDateRange) == 1 ? "SUBSTRING(timeInterval, 12,13) + ' hrs'" : "SUBSTRING(timeInterval, 1,10)";
 
          let arrLock = [];
          let userData = alasql(`
             SELECT
                CASE 
-                  WHEN diagcode IN ('D0003','D0004','D0005','D0007','D0009','D0012','D0015') THEN 'Locked'
-                  WHEN diagcode IN ('D0002','D0006','D0008','D0010','D0011','D0013','D0014','D0001') THEN 'Unlocked'
-               END [lockStatus]
+                  WHEN diagcode IN ('D0001','D0002','D0006','D0014','D0011','D0010','D0013') THEN 0
+                  WHEN diagcode IN ('D0003','D0004','D0005','D0007','D0008','D0009','D0012','D0015') THEN 1
+               END [locked]
                ,userid
-               ,SUBSTRING(stamp, 1, 16) date
+               ,stamp [date]
+               ${timeInterval}
             FROM ? 
-            WHERE utc >= '${ dateFrom }' AND utc <='${ dateTo }' 
-            ${ slctUserId !== "" ? " AND userid = '" + slctUserId + "'" : "" }
+            WHERE utc >= '${dateFrom}' AND utc <='${dateTo}' 
+            ${slctUserId !== "" ? " AND userid = '" + slctUserId + "'" : ""}
             ORDER BY userid, stamp
          `, [result]);
-         
-         userData.forEach(function(d,idx){d.rownum = idx});
+
+         userData.forEach(function (d, idx) { d.rownum = idx });
+
+         // console.table(userData);
 
          for (let i = 0; i < userData.length; i++) {
             let actualData = userData[i];
 
-            if (i != userData.length - 1) {
-               let nextData = userData[i + 1];   
-               if (actualData.lockStatus != nextData.lockStatus) {
-                  arrLock.push(actualData);
+            if (i > 0) {
+               let nextData = userData[i + 1];
+               if (nextData !== undefined && (actualData.locked != nextData.locked || actualData.userid != nextData.userid)) {
+                  arrLock.push(nextData);
                }
             }
-            else 
+            else
                arrLock.push(actualData);
          }
-         
+
          for (let i = 0; i < arrLock.length; i++) {
             let actualData = arrLock[i];
 
             if (i != arrLock.length - 1) {
-               let nextData = arrLock[i + 1];   
+               let nextData = arrLock[i + 1];
                if (actualData.userid == nextData.userid) {
-                  if (actualData.lockStatus == "Unlocked") { // ACTIVE TIME
-                     arrLock[i].activeTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
-                     arrLock[i].inactiveTime = 0;
+                  if (actualData.locked == 0) { // UNLOCKED TIME
+                     arrLock[i].unlockedTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
+                     arrLock[i].lockedTime = 0;
                   }
-                  else { // INACTIVE TIME
-                     arrLock[i].activeTime = 0
-                     arrLock[i].inactiveTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
+                  else { // LOCKED TIME
+                     arrLock[i].unlockedTime = 0
+                     arrLock[i].lockedTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
                   }
                }
             }
          }
 
-         // console.table(arrLock);
-
+         //console.table(arrLock); // TIMES
+         
          let activeInactiveData = alasql(`
-            SELECT userid, SUBSTRING(date, 1, 10) [date], SUM(activeTime) activeTime, SUM(inactiveTime) inactiveTime
+            SELECT  SUBSTRING(date, 1, 10) [date], SUM(unlockedTime) unlockedMs, SUM(lockedTime) lockedMs,
+               ${timeIntervalFormat} AS [timeInterval]
             FROM ? 
-            GROUP BY userid, SUBSTRING(date, 1, 10)
+            GROUP BY SUBSTRING(date, 1, 10), ${timeIntervalFormat}
+            ORDER BY [timeInterval]
          `, [arrLock])
 
          activeInactiveData.forEach(item => {
-            item.ActiveHours = msToTime(item.activeTime);
-            item.InactiveHours = msToTime(item.inactiveTime);
-         })
-
-         console.table(activeInactiveData);
-
-         if(dataTable !== null)
-            dataTable.destroy();
-
-         dataTable = $('#example').DataTable({
-            data: activeInactiveData,
-            destroy: true,
-            columns: [
-               { data: "userid" },
-               { data: "date" },
-               { data: "ActiveHours" },
-               { data: "InactiveHours" },
-               { data: "activeTime" },
-            ],
-            columnDefs: [
-               {
-                  "targets": [4],
-                  "visible": false,
-                  "searchable": false
-               }
-            ],
-            order: [[1, "desc"]],
-            // rowCallback: function (row, data, index) {
-            //    if ("data['key'] != 'valid key'") {
-            //       $(row).find('td:eq(0)').css('color', 'red');
-            //    }
-            // },
-            // rowCallback: function (row, data, index) {
-            //    $(row).find('td:eq(0)').html(`<a style="color: #000000;" href="users.html?usrid=${data['userid']}">${data['userid']}</a>`);
-            //    $(row).find('td:eq(1)').html(`<a style="color: #000000;" href="machines.html?mchname=${data['machinename']}">${data['machinename']}</a>`);
-            //    $(row).find('td:eq(6)').html(formatDate(data['stamp']));
-            //    $(row).find('td:eq(7)').html(getCode(data['diagcode']));
-            // },
-            select: true,
-            pageLength: 10,
-            responsive: true,
-            dom: '<"top"i>rt<"bottom"flp><"html5buttons"B><"clear">',
-            retrieve: true,
-            searching: false,
-            buttons: [
-               { extend: 'copy' },
-               { extend: 'csv' },
-               { extend: 'excel', title: 'ExampleFile' },
-               { extend: 'pdf', title: 'ExampleFile' },
-               {
-                  extend: 'print',
-                  customize: function (win) {
-                     $(win.document.body).addClass('white-bg');
-                     $(win.document.body).css('font-size', '10px');
-
-                     $(win.document.body).find('table')
-                        .addClass('compact')
-                        .css('font-size', 'inherit');
-                  }
-               }
-            ]
+            item.UnlockedTime = msToTime(item.unlockedMs);
+            item.LockedTime = msToTime(item.lockedMs);
          });
 
+         //console.table(activeInactiveData); // FINAL RESULT
+
+         createGraph(activeInactiveData);
       },
       error: function (err) {
          console.error(err);
@@ -188,19 +136,205 @@ function getproductivityData() {
    });
 }
 
+function createGraph(graphData) {
+   var barData = {
+      labels: getGraphLabels(graphData),
+      datasets: [
+         {
+            label: "Locked",
+            backgroundColor: "rgba(220, 220, 220, 0.5)",
+            pointBorderColor: "#fff",
+            data: getGraphLockValues(graphData),
+         },
+         {
+            label: "Unlocked",
+            backgroundColor: "rgba(26,179,148,0.5)",
+            borderColor: "rgba(26,179,148,0.7)",
+            pointBackgroundColor: "rgba(26,179,148,1)",
+            pointBorderColor: "#fff",
+            data: getGraphUnlockValues(graphData),
+         },
+      ]
+   };
+
+   var barOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+         yAxes: [
+            {
+               ticks: {
+                  beginAtZero: true,
+                  callback: function (label, index, labels) {
+                     return msToTime(label);
+                  }
+               }
+            }
+         ]
+      },
+      tooltips: {
+         callbacks: {
+            label: function (tooltipItem) {
+               return msToTime(tooltipItem.yLabel);
+            }
+         }
+      },
+   };
+
+   if (ctx2 !== null)
+      ctx = null;
+   if (chart !== null)
+      chart.destroy();
+      
+   ctx2 = document.getElementById("barChart").getContext("2d");
+   chart = new Chart(ctx2, { type: "bar", data: barData, options: barOptions });
+}
+
+function getGraphLabels(data) {
+   let result = [];
+   data.forEach(item => {
+      result.push(item.timeInterval);
+   });
+   return result;
+}
+
+function getGraphLockValues(data) {
+   let result = [];
+   data.forEach(item => {
+      result.push(item.lockedMs);
+   });
+   return result;
+}
+
+function getGraphUnlockValues(data) {
+   let result = [];
+   data.forEach(item => {
+      result.push(item.unlockedMs);
+   });
+   return result;
+}
+
+function formatDate(utcDate) {
+   let d = new Date(utcDate);
+   let month =
+      d.getMonth().toString().length == 1 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1);
+   let day =
+      d.getDate().toString().length == 1 ? "0" + d.getDate() : d.getDate();
+   let hour =
+      d.getHours().toString().length == 1 ? "0" + d.getHours() : d.getHours();
+   let min =
+      d.getMinutes().toString().length == 1
+         ? "0" + d.getMinutes()
+         : d.getMinutes();
+   let sec =
+      d.getSeconds().toString().length == 1
+         ? "0" + d.getSeconds()
+         : d.getSeconds();
+   return (
+      d.getFullYear() +
+      "-" +
+      month +
+      "-" +
+      day +
+      " " +
+      hour +
+      ":" +
+      min +
+      ":" +
+      sec
+   );
+}
 
 function msToTime(s) {
    function pad(n, z) {
-     z = z || 2;
-     return ('00' + n).slice(-z);
+      z = z || 2;
+      return ('00' + n).slice(-z);
    }
- 
+
    var ms = s % 1000;
    s = (s - ms) / 1000;
    var secs = s % 60;
    s = (s - secs) / 60;
    var mins = s % 60;
    var hrs = (s - mins) / 60;
- 
-   return pad(hrs) + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
- }
+
+   return pad(hrs) + ':' + pad(mins) + ':' + pad(secs);
+}
+
+function msToDateTime(s) {
+   function pad(n, z) {
+      z = z || 2;
+      return ('00' + n).slice(-z);
+   }
+
+   var ms = s % 1000;
+   s = (s - ms) / 1000;
+   var secs = s % 60;
+   s = (s - secs) / 60;
+   var mins = s % 60;
+   var hrs = (s - mins) / 60;
+
+   let dateNow = new Date();
+   return new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate(), pad(hrs), pad(mins), pad(secs), 0);
+}
+
+function getTimeInterval(range) {
+   switch (parseInt(range)) {
+      case 1:
+         return ",SUBSTRING(stamp, 1, 13) [timeInterval]";
+      case 7:
+      case 14:
+      case 30:
+         return ",SUBSTRING(stamp, 1, 10) [timeInterval]";
+      default:
+         console.log(range);
+         break;
+   }
+}
+
+
+//if (dataTable !== null)
+//    dataTable.destroy();
+
+// dataTable = $('#example').DataTable({
+//    data: activeInactiveData,
+//    destroy: true,
+//    columns: [
+//       { data: "userid" },
+//       { data: "date" },
+//       { data: "ActiveHours" },
+//       { data: "InactiveHours" },
+//       { data: "activeTime" },
+//    ],
+//    columnDefs: [
+//       {
+//          "targets": [4],
+//          "visible": false,
+//          "searchable": false
+//       }
+//    ],
+//    order: [[1, "desc"]],
+//    select: true,
+//    pageLength: 10,
+//    responsive: true,
+//    dom: '<"top"i>rt<"bottom"flp><"html5buttons"B><"clear">',
+//    retrieve: true,
+//    searching: false,
+//    buttons: [
+//       { extend: 'copy' },
+//       { extend: 'csv' },
+//       { extend: 'excel', title: 'ExampleFile' },
+//       { extend: 'pdf', title: 'ExampleFile' },
+//       {
+//          extend: 'print',
+//          customize: function (win) {
+//             $(win.document.body).addClass('white-bg');
+//             $(win.document.body).css('font-size', '10px');
+
+//             $(win.document.body).find('table')
+//                .addClass('compact')
+//                .css('font-size', 'inherit');
+//          }
+//       }
+//    ]
+// });
--- js/demo/chartjs-pro.js
+++ js/demo/chartjs-pro.js
@@ -1,32 +1,29 @@
 $(function () {
+   var barData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+         {
+            label: "Locked",
+            backgroundColor: "rgba(220, 220, 220, 0.5)",
+            pointBorderColor: "#fff",
+            data: [25, 29, 40, 50, 30, 50, 30],
+         },
+         {
+            label: "Unlocked",
+            backgroundColor: "rgba(26,179,148,0.5)",
+            borderColor: "rgba(26,179,148,0.7)",
+            pointBackgroundColor: "rgba(26,179,148,1)",
+            pointBorderColor: "#fff",
+            data: [28, 48, 40, 19, 36, 27, 45],
+         },
+      ],
+   };
 
-    var barData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "inactive",
-                    backgroundColor: 'rgba(220, 220, 220, 0.5)',
-                    pointBorderColor: "#fff",
-                    data: [25, 29, 40, 50, 30, 50, 30]
-                },
-                {
-                    label: "active",
-                    backgroundColor: 'rgba(26,179,148,0.5)',
-                    borderColor: "rgba(26,179,148,0.7)",
-                    pointBackgroundColor: "rgba(26,179,148,1)",
-                    pointBorderColor: "#fff",
-                    data: [28, 48, 40, 19, 36, 27, 45]
-                }
-            ]
-        };
+   var barOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+   };
 
-        var barOptions = {
-            responsive: true,
-            maintainAspectRatio: false,
-        };
-
-
-        var ctx2 = document.getElementById("barChart").getContext("2d");
-        new Chart(ctx2, {type: 'bar', data: barData, options:barOptions});
-
+   var ctx2 = document.getElementById("barChart").getContext("2d");
+   new Chart(ctx2, { type: "bar", data: barData, options: barOptions });
 });
