ae8c6a4de81227f5c48ddf1ba5b86e38105e6532
Ricardo Villa Sanchez <ing_ricardo.villa@hotmail.com>
2021-07-08 23:09:58 -0500

    Productivity graph fix

--- js/app/productivity.js
+++ js/app/productivity.js
@@ -1,6 +1,7 @@
 var dataTable = null;
 var ctx2 = null;
 var chart = null;
+var selectedUsers = [];
 
 Date.prototype.addDays = function (days) {
    var date = new Date(this.valueOf());
@@ -27,13 +28,16 @@ $.ajax({
 
       let slctUsersHtml = `<option value=""></option>`;
 
+      // slctUsersHtml += `<option value="ALL">Select All</option>`;
       usersData.forEach(user => {
          slctUsersHtml += `
-            <option value="${user.userid}">${user.userid}</option>
+            <option value="${user.userid}">${user.userid.toUpperCase()}</option>
          `;
       });
 
       $("#slctUserId").html(slctUsersHtml);
+      $("#slctUserId > option").prop("selected", "selected");
+      $("#slctUserId").trigger("change");
    },
    error: function (request, status, error) {
       console.error(error);
@@ -54,28 +58,58 @@ function getproductivityData() {
       success: function (result) {
          let slctDateRange = document.getElementById("slctDateRange").value == "" ? 1
             : document.getElementById("slctDateRange").value;
-         let slctUserId = document.getElementById("slctUserId").value;
-         let dateFrom = formatDate(new Date().addDays(-slctDateRange));
-         let dateTo = formatDate(new Date());
+         let dateFrom = formatDate(new Date().addDays(-slctDateRange)).toString().replace(" ", "T");
+         let dateTo = formatDate(new Date()).toString().replace(" ", "T");
          let timeInterval = getTimeInterval(slctDateRange);
          let timeIntervalFormat = parseInt(slctDateRange) == 1 ? "SUBSTRING(timeInterval, 12,13) + ' hrs'" : "SUBSTRING(timeInterval, 1,10)";
-
+         let slctUsersId = "";
          let arrLock = [];
+         let dates = [];
+         let labelData = {
+            totalEmployes: 0,
+            avgHours: 0,
+            mostActive: "",
+            leastActive: ""
+         }
+
+         let usersData = alasql(`
+               SELECT userid
+               FROM ?
+               GROUP BY userid
+            `, [result]);
+         labelData.totalEmployes = usersData.length;
+
+         if (selectedUsers.length == 0) {
+            usersData.forEach(user => selectedUsers.push(user.userid));
+            selectedUsers.forEach(user => slctUsersId += `'${user}',`);
+         } else
+            selectedUsers.forEach(user => slctUsersId += `'${user}',`);
+
+         if (slctDateRange == 1) {
+            dates.push(dateFrom.substring(0, 10));
+            dates.push(dateTo.substring(0, 10));
+         }
+         else
+            for (let i = 0; i < slctDateRange; i++) {
+               let dt = formatDate(new Date().addDays(-i)).toString();
+               dates.push(dt.substring(0, 10));
+            }
+
          let userData = alasql(`
             SELECT
                CASE 
-                  WHEN diagcode IN ('D0001','D0002','D0006','D0014','D0011','D0010','D0013') THEN 0
-                  WHEN diagcode IN ('D0003','D0004','D0005','D0007','D0008','D0009','D0012','D0015') THEN 1
-               END [locked]
+                  WHEN diagcode IN ('D0001','D0002','D0006','D0014','D0011','D0010','D0013') THEN 1
+                  WHEN diagcode IN ('D0003','D0004','D0005','D0007','D0008','D0009','D0012','D0015') THEN 0
+                  ELSE  diagcode
+               END [activeStatus]
                ,userid
                ,stamp [date]
                ${timeInterval}
             FROM ? 
-            WHERE utc >= '${dateFrom}' AND utc <='${dateTo}' 
-            ${slctUserId !== "" ? " AND userid = '" + slctUserId + "'" : ""}
+            WHERE stamp >= '${dateFrom}' AND stamp <='${dateTo}' 
+            AND userid IN (${slctUsersId}'')
             ORDER BY userid, stamp
          `, [result]);
-
          userData.forEach(function (d, idx) { d.rownum = idx });
 
          // console.table(userData);
@@ -85,7 +119,7 @@ function getproductivityData() {
 
             if (i > 0) {
                let nextData = userData[i + 1];
-               if (nextData !== undefined && (actualData.locked != nextData.locked || actualData.userid != nextData.userid)) {
+               if (nextData !== undefined && (actualData.activeStatus != nextData.activeStatus || actualData.userid != nextData.userid)) {
                   arrLock.push(nextData);
                }
             }
@@ -99,34 +133,83 @@ function getproductivityData() {
             if (i != arrLock.length - 1) {
                let nextData = arrLock[i + 1];
                if (actualData.userid == nextData.userid) {
-                  if (actualData.locked == 0) { // UNLOCKED TIME
-                     arrLock[i].unlockedTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
-                     arrLock[i].lockedTime = 0;
+                  if (actualData.activeStatus == 1) { // ACTIVE TIME
+                     arrLock[i].activeTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
+                     arrLock[i].inactiveTime = 0;
                   }
-                  else { // LOCKED TIME
-                     arrLock[i].unlockedTime = 0
-                     arrLock[i].lockedTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
+                  else { // INACTIVE TIME
+                     arrLock[i].activeTime = 0
+                     arrLock[i].inactiveTime = Math.abs(new Date(nextData.date) - new Date(actualData.date));
                   }
                }
             }
          }
 
-         //console.table(arrLock); // TIMES
-         
+         if (arrLock.length > 0) {
+            let date = arrLock[0].date;
+            let userId = arrLock[0].userid;
+            let actualHour = new Date().getHours()
+
+            for (let dt = 0; dt < dates.length; dt++) {
+               if (parseInt(slctDateRange) === 1) {
+                  for (let hr = 0; hr < 24; hr++) {
+                     if ((dt == 0 && hr >= actualHour) || (dt == 1 && hr <= actualHour)) {
+                        let timeI = date.substring(0, 11);
+                        arrLock.push({
+                           activeStatus: 1,
+                           activeTime: 0,
+                           date: dates[dt],
+                           inactiveTime: 0,
+                           rownum: 0,
+                           timeInterval: hr.toString().length == 1 ? timeI + "0" + hr.toString() : timeI + hr.toString(),
+                           userid: userId
+                        });
+                     }
+                  }
+               }
+               else {
+                  arrLock.push({
+                     activeStatus: 1,
+                     activeTime: 0,
+                     date: dates[dt],
+                     inactiveTime: 0,
+                     rownum: 0,
+                     timeInterval: dates[dt],
+                     userid: userId
+                  });
+               }
+            }
+         }
+         // console.table(arrLock); // TIMES
+
          let activeInactiveData = alasql(`
-            SELECT  SUBSTRING(date, 1, 10) [date], SUM(unlockedTime) unlockedMs, SUM(lockedTime) lockedMs,
+            SELECT SUBSTRING(date, 1, 10) [date], SUM(activeTime) activeMs, SUM(inactiveTime) inactiveMs,
                ${timeIntervalFormat} AS [timeInterval]
             FROM ? 
             GROUP BY SUBSTRING(date, 1, 10), ${timeIntervalFormat}
-            ORDER BY [timeInterval]
+            ORDER BY [date], [timeInterval]
          `, [arrLock])
 
          activeInactiveData.forEach(item => {
-            item.UnlockedTime = msToTime(item.unlockedMs);
-            item.LockedTime = msToTime(item.lockedMs);
+            item.activeTime = msToTime(item.activeMs);
+            item.inactiveTime = msToTime(item.inactiveMs);
          });
 
-         //console.table(activeInactiveData); // FINAL RESULT
+         // console.table(activeInactiveData); // FINAL RESULT
+         if (arrLock.length > 0) {
+            let mostleastData = alasql(`SELECT COUNT(userid) [userCount], userid FROM ? GROUP BY userid`, [arrLock]);
+            labelData.avgHours = msToTime(alasql(`SELECT AVG(activeMs) [avgHours] FROM ?`, [activeInactiveData])[0].avgHours);
+            labelData.mostActive = alasql(`SELECT TOP 1 userCount, userid FROM ? ORDER BY userCount DESC`, [mostleastData])[0].userid;
+            labelData.leastActive = alasql(`SELECT TOP 1 userCount, userid FROM ? ORDER BY userCount ASC`, [mostleastData])[0].userid;
+         }
+
+         let lableGraphHtml = `
+               <p><b>Total Number of Employees: </b>${labelData.totalEmployes}</p>
+               <p><b>Average Hous: </b>${labelData.avgHours}</p>
+               <p><b>Most Active: </b>${ labelData.mostActive == "" ? "" : labelData.mostActive.toUpperCase()}</p>
+               <p><b>Least Active: </b>${ labelData.leastActive == "" ? "" : labelData.leastActive.toUpperCase()}</p>
+            `;
+            $("#label-graph-info").html(lableGraphHtml);
 
          createGraph(activeInactiveData);
       },
@@ -136,18 +219,26 @@ function getproductivityData() {
    });
 }
 
+$(".select_multiuser").select2MultiCheckboxes({
+   placeholder: "Select users",
+   templateSelection: function (selected, total) {
+      selectedUsers = selected;
+      return "Selected " + (selected.length - 1) + " of " + (total - 1);
+   }
+});
+
 function createGraph(graphData) {
    var barData = {
       labels: getGraphLabels(graphData),
       datasets: [
          {
-            label: "Locked",
+            label: "Inactive",
             backgroundColor: "rgba(220, 220, 220, 0.5)",
             pointBorderColor: "#fff",
             data: getGraphLockValues(graphData),
          },
          {
-            label: "Unlocked",
+            label: "Active",
             backgroundColor: "rgba(26,179,148,0.5)",
             borderColor: "rgba(26,179,148,0.7)",
             pointBackgroundColor: "rgba(26,179,148,1)",
@@ -185,7 +276,7 @@ function createGraph(graphData) {
       ctx = null;
    if (chart !== null)
       chart.destroy();
-      
+
    ctx2 = document.getElementById("barChart").getContext("2d");
    chart = new Chart(ctx2, { type: "bar", data: barData, options: barOptions });
 }
@@ -201,7 +292,7 @@ function getGraphLabels(data) {
 function getGraphLockValues(data) {
    let result = [];
    data.forEach(item => {
-      result.push(item.lockedMs);
+      result.push(item.inactiveMs);
    });
    return result;
 }
@@ -209,7 +300,7 @@ function getGraphLockValues(data) {
 function getGraphUnlockValues(data) {
    let result = [];
    data.forEach(item => {
-      result.push(item.unlockedMs);
+      result.push(item.activeMs);
    });
    return result;
 }
@@ -258,7 +349,7 @@ function msToTime(s) {
    var mins = s % 60;
    var hrs = (s - mins) / 60;
 
-   return pad(hrs) + ':' + pad(mins) + ':' + pad(secs);
+   return pad(hrs) + ':' + pad(mins)
 }
 
 function msToDateTime(s) {
