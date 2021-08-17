28bf9b530fc1507fb82aa930e43c5521210644e0
Ricardo Villa Sanchez <ing_ricardo.villa@hotmail.com>
2021-08-17 10:55:15 -0500

    Admin and Menu development

--- js/app/general.js
+++ js/app/general.js
@@ -1,107 +1,154 @@
 $(document).ready(function () {
    createMenu();
-   
-   function createMenu() {
 
+   function createMenu() {
       const path = window.location.pathname.substring(1);
-      console.log(path);
+      pathLen = path.split("/");
 
       let navMenu = `
-         <ul class="nav navbar-nav mr-auto">
-            <li class="sub-menu ${ path == 'indexMain.html' ? 'active' : '' }">
-               <a href='indexMain.html'>
-                  <span>Home</span>
-               </a>
-            </li>
+      <ul class="nav navbar-nav mr-auto">
+         <li class="sub-menu ${path == "indexMain.html" ? "active" : ""}">
+            <a href='${(pathLen.length > 1 ? "../" : "") + "indexMain.html"
+         }'>
+               <span>Home</span>
+            </a>
+         </li>
 
-            <li class="sub-menu ${ path == 'productivity.html' ? 'active' : '' }">
-               <a href='productivity.html'>
-                  <span>Productivity</span>
-               </a>
-            </li>
+         <li class="sub-menu ${path == "productivity.html" ? "active" : ""}">
+            <a href='${(pathLen.length > 1 ? "../" : "") + "productivity.html"
+         }'>
+               <span>Productivity</span>
+            </a>
+         </li>
 
-            <li class="sub-menu ${ path == 'logs.html' ? 'active' : '' }">
-               <a href='logs.html'>
-                  <span>Logs</span>
-               </a>
-            </li>
+         <li class="sub-menu ${path == "logs.html" ? "active" : ""}">
+            <a href='${(pathLen.length > 1 ? "../" : "") + "logs.html"}'>
+               <span>Logs</span>
+            </a>
+         </li>
 
-            <li class='sub-menu'>
-               <a href='#'>
-                  <span>Reports</span>
-                  <i class='fa fa-angle-down'></i>
-               </a>
-               <ul>
-                  <li>
-                     <a href='#'>
-                        Application
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        User Machine
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        Access Logs
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        Gps Locations
-                     </a>
-                  </li>
-               </ul>
-            </li>
+         <li class='sub-menu'>
+            <a href='#'>
+               <span>Reports</span>
+               <i class='fa fa-angle-down'></i>
+            </a>
+            <ul>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "reports/"
+            : pathLen[0] !== "reports"
+               ? "../reports/"
+               : "") + "application.html"
+         }'>
+                     Application
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "reports/"
+            : pathLen[0] !== "reports"
+               ? "../reports/"
+               : "") + "userMachine.html"
+         }'>
+                     User Machine
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "reports/"
+            : pathLen[0] !== "reports"
+               ? "../reports/"
+               : "") + "accessLogs.html"
+         }'>
+                     Access Logs
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "reports/"
+            : pathLen[0] !== "reports"
+               ? "../reports/"
+               : "") + "gpsLocations.html"
+         }'>
+                     Gps Locations
+                  </a>
+               </li>
+            </ul>
+         </li>
 
-            <li class='sub-menu'>
-               <a href='#'>
-                  <span>Admin</span>
-                  <i class='fa fa-angle-down'></i>
-               </a>
-               <ul>
-                  <li>
-                     <a href='#'>
-                        User Access Management
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        Security Logs
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        User / Machine Management
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        Report Admin
-                     </a>
-                  </li>
-                  <li>
-                     <a href='#'>
-                        Location Profiles MGMT
-                     </a>
-                  </li>
-               </ul>
-            </li>        
-         </ul>
-         <ul class="nav navbar-top-links navbar-right">
-            <li>
-               <a href="login.html">
-                  <i class="fa fa-sign-out"></i> Log out
-               </a>
-            </li>
-         </ul>`;
+         <li class='sub-menu'>
+            <a href='#'>
+               <span>Admin</span>
+               <i class='fa fa-angle-down'></i>
+            </a>
+            <ul>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "admin/"
+            : pathLen[0] !== "admin"
+               ? "../admin/"
+               : "") + "userAccessManagement.html"
+         }'>
+                     User Access Management
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "admin/"
+            : pathLen[0] !== "admin"
+               ? "../admin/"
+               : "") + "securityLogs.html"
+         }'>
+                     Security Logs
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "admin/"
+            : pathLen[0] !== "admin"
+               ? "../admin/"
+               : "") + "userMachineManagement.html"
+         }'>
+                     User / Machine Management
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "admin/"
+            : pathLen[0] !== "admin"
+               ? "../admin/"
+               : "") + "reportAdmin.html"
+         }'>
+                     Report Admin
+                  </a>
+               </li>
+               <li>
+                  <a href='${(pathLen.length == 1
+            ? "admin/"
+            : pathLen[0] !== "admin"
+               ? "../admin/"
+               : "") + "locationProfiles.html"
+         }'>
+                     Location Profiles MGMT
+                  </a>
+               </li>
+            </ul>
+         </li>        
+      </ul>
+      <ul class="nav navbar-top-links navbar-right">
+         <li>
+            <a href="login.html">
+               <i class="fa fa-sign-out"></i> Log out
+            </a>
+         </li>
+      </ul>`;
 
       $(".dinamyc-nav-menu").html(navMenu);
    }
 
    $(".sub-menu ul").hide();
    $(".sub-menu a").click(function () {
+      $(".sub-menu ul").hide();
       $(this).parent(".sub-menu").children("ul").slideToggle("200");
       $(this).find("i.fa").toggleClass("fa-angle-up fa-angle-down");
    });
